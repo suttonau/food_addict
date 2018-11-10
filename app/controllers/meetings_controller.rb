199c9b44a42fe86d3bc30c4faf25a3572386b969
suttonau <sutton_mjbc@icloud.com>
2018-11-10 03:17:19 -0500

    crud for meetings cntrler

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -9,4 +9,22 @@ class MeetingsController < ApplicationController
     render json: Meeting.find(params[:id])
   end
 
+  def create
+    render json: Meeting.create(meeting_params)
+  end
+
+  def update
+    render json: Meeting.find(params[:id]).update(meeting_params)
+  end
+
+  def destroy
+    render json: Meeting.find(params[:id].destroy
+  end
+
+  private
+
+  def meeting_params
+    params.require(:meeting).permit(:name)
+  end
+
 end
