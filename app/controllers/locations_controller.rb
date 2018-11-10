63761b76be5647684454fccc13b92d5f88f0069d
suttonau <sutton_mjbc@icloud.com>
2018-11-10 01:33:38 -0500

    setting up routes

---
+++ app/controllers/locations_controller.rb
@@ -0,0 +1,10 @@
+class LocationsController < ApplicationController
+
+  def index
+    render json: Location.all
+  end
+
+  def show
+    render json: Location.find(params[:id])
+  end
+end
--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,2 +1,11 @@
 class MeetingsController < ApplicationController
+
+  def index
+    render json: Meeting.all
+  end
+
+  def show
+    render json: Meeting.find(params[:id])
+  end
+
 end
